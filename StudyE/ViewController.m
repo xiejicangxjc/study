64e13d8cbef7e95e7984e55feb892c2ba7d4ec9d
damai <xiejicangxjc@126.com>
2017-09-09 11:33:01 +0800

    hehe

--- StudyE/ViewController.m
+++ StudyE/ViewController.m
@@ -16,13 +16,82 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    /*
+     
+     
+     
+     
+     ksjfaklsfaskldfjlaskdfjklsadkla;
+     
+     
+     
+     */
     
     
-    
-    
+    for (int i = 0 ; i < 10 ; i ++) {
+        switch (i) {
+            case 0:
+                
+                break;
+                case 1:
+                
+                break;
+                case 2:
+                
+                break;
+                case 3:
+                
+                break;
+                case 4:
+                
+                break;
+                case 5:
+                
+                break;
+                case 6:
+                
+                break;
+                case 7:
+                
+                break;
+                case 8:
+                
+                break;
+                case 9:
+                
+                break;
+            default:
+                break;
+        }
+    }
     
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @end
